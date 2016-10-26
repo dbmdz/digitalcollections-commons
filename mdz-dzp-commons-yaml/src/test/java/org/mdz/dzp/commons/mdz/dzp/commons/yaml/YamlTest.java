682ef8bb6fff7d1b8bdb8ae9639fcf827db41b18
Marcus Bitzl <marcus.bitzl@bsb-muenchen.de>
2016-10-26 15:15:49 +0200

    Added tests.

---
+++ mdz-dzp-commons-yaml/src/test/java/org/mdz/dzp/commons/mdz/dzp/commons/yaml/YamlTest.java
@@ -0,0 +1,30 @@
+package org.mdz.dzp.commons.mdz.dzp.commons.yaml;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import org.joda.time.DateTime;
+import org.junit.Before;
+import org.junit.Test;
+import org.mdz.dzp.commons.mdz.dzp.commons.yaml.joda.JodaTimeConstructor;
+import org.mdz.dzp.commons.mdz.dzp.commons.yaml.joda.JodaTimeRepresenter;
+import org.yaml.snakeyaml.DumperOptions;
+import org.yaml.snakeyaml.Yaml;
+
+public class YamlTest {
+
+  private Yaml yaml;
+
+  @Before
+  public void setUp() {
+    DumperOptions options = new DumperOptions();
+    options.setDefaultFlowStyle(DumperOptions.FlowStyle.FLOW);
+    options.setSplitLines(false);
+    yaml = new Yaml(new JodaTimeConstructor(), new JodaTimeRepresenter(), options);
+  }
+
+  @Test
+  public void shouldSerializeAndDeserializeDateTime() {
+    DateTime dateTime = DateTime.now();
+    assertThat(yaml.load(yaml.dump(dateTime))).isEqualTo(dateTime);
+  }
+
+}
