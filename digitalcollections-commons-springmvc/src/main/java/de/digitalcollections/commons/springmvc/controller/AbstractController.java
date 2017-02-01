51ad712fca79c0ede116b170d4e9a67e9430c807
Ralf Eichinger <ralf.eichinger@bsb-muenchen.de>
2017-02-01 17:28:20 +0100

    added abstractcontroller and aop logging

---
+++ digitalcollections-commons-springmvc/src/main/java/de/digitalcollections/commons/springmvc/controller/AbstractController.java
@@ -0,0 +1,15 @@
+package de.digitalcollections.commons.springmvc.controller;
+
+import org.springframework.util.StringUtils;
+import org.springframework.validation.BindingResult;
+
+public abstract class AbstractController {
+
+  protected void verifyBinding(BindingResult br) {
+    String[] suppressedFields = br.getSuppressedFields();
+    if (suppressedFields.length > 0) {
+      throw new RuntimeException("Attempting to bind suppressed fields: "
+              + StringUtils.arrayToCommaDelimitedString(suppressedFields));
+    }
+  }
+}
