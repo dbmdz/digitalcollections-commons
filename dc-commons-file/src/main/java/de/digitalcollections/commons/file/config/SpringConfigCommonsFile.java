38f652b3b9a26ef8192517681c81e05e74523a42
Ralf Eichinger <ralf.eichinger@bsb-muenchen.de>
2019-02-22 16:38:39 +0100

    Fix component scan path of services

--- dc-commons-file/src/main/java/de/digitalcollections/commons/file/config/SpringConfigCommonsFile.java
+++ dc-commons-file/src/main/java/de/digitalcollections/commons/file/config/SpringConfigCommonsFile.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 @ComponentScan(basePackages = {
   "de.digitalcollections.commons.file.backend.impl",
-  "de.digitalcollections.commons.file.business.impl.service"
+  "de.digitalcollections.commons.file.business.impl"
 })
 @EnableConfigurationProperties
 public class SpringConfigCommonsFile {
