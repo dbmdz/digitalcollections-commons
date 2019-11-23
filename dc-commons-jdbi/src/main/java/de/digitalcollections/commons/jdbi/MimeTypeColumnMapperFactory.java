c5b7a7ae762e0d624949caccd24790a56cefd767
ralf <ralf@fujitsu>
2019-11-23 13:57:11 +0100

    Add null check in reading mimetype from database

--- dc-commons-jdbi/src/main/java/de/digitalcollections/commons/jdbi/MimeTypeColumnMapperFactory.java
+++ dc-commons-jdbi/src/main/java/de/digitalcollections/commons/jdbi/MimeTypeColumnMapperFactory.java
@@ -13,7 +13,11 @@ public class MimeTypeColumnMapperFactory implements ColumnMapperFactory {
   public Optional<ColumnMapper<?>> build(Type type, ConfigRegistry config) {
     if (type == MimeType.class) {
       return Optional.of((resultSet, columnNumber, statementContext) -> {
-        return MimeType.fromTypename(resultSet.getString(columnNumber));
+        final String typename = resultSet.getString(columnNumber);
+        if (typename == null) {
+          return null;
+        }
+        return MimeType.fromTypename(typename);
       });
     }
     return Optional.empty();
