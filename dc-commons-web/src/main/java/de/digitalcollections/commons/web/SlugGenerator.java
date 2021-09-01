5063555d7e5fbb41d8b352490e67d7e0bf272062
Christoph Lorenz <christoph.lorenz@bsb-muenchen.de>
2021-09-01 14:53:39 +0200

    Introducing dc-commons-web with the SlugGenerator

---
+++ dc-commons-web/src/main/java/de/digitalcollections/commons/web/SlugGenerator.java
@@ -0,0 +1,57 @@
+package de.digitalcollections.commons.web;
+
+import com.ibm.icu.text.Transliterator;
+import java.util.Locale;
+
+/**
+ * Helper class to transform "special" characters into a slug compatible and human-readable form.
+ */
+public class SlugGenerator {
+
+  private final Transliterator transliterator;
+
+  /**
+   * Default constructor for transliteration of german umlauts and further latin characters
+   */
+  public SlugGenerator() {
+    this("Any-Latin; de-ASCII");
+  }
+
+  /**
+   * Constructor, if other transliteration than german umlauts and further latin characters is required.
+   * @param transliteratorId the ids for the transliteration charset(s)
+   * @see <a href="https://unicode-org.github.io/icu-docs/apidoc/dev/icu4j/com/ibm/icu/text/Transliterator.html">https://unicode-org.github.io/icu-docs/apidoc/dev/icu4j/com/ibm/icu/text/Transliterator.html</a>
+   */
+  public SlugGenerator(String transliteratorId) {
+    transliterator = Transliterator.getInstance(transliteratorId);
+  }
+
+  /**
+   * Build a slug for a given string.
+   *
+   * <p>This is done by replacing all characters outside [A-Za-z0-9] by a dash, by avoiding
+   * repeating dashes and dashes at the beginning and at the end. Additionally, special characters
+   * like german umlauts are transformed into their base form. Last but not least, the calculated
+   * slug is transformed to lowercase
+   *
+   * @param string as input
+   * @return slug in a web-compatible but human readable form
+   */
+  public String generateSlug(String string) {
+    if (string==null || string.isBlank()) {
+      return string;
+    }
+
+    // Transform umlauts and others
+    String slug = transliterator.transliterate(string);
+    // All non A-Za-z0-9 characters are replaced by a dash
+    slug = slug.replaceAll("[^\\p{IsAlphabetic}\\p{IsDigit}-]", "-");
+    // Repeating dashes are removed
+    slug = slug.replaceAll("--", "-");
+    // Remove initial and final dashes
+    slug = slug.replaceAll("^-", "").replaceAll("-$", "");
+    // Transform string to lowercase
+    slug = slug.toLowerCase(Locale.ROOT);
+    return slug;
+  }
+}
---
+++ dc-commons-web/src/test/java/de/digitalcollections/commons/web/SlugGeneratorTest.java
@@ -0,0 +1,61 @@
+package de.digitalcollections.commons.web;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+@DisplayName("The slug generator")
+class SlugGeneratorTest {
+
+  private SlugGenerator slugGenerator;
+
+  @BeforeEach
+  public void beforeEach() {
+    slugGenerator = new SlugGenerator();
+  }
+
+  @DisplayName("returns null for a null input")
+  @Test
+  public void returnsNullForNull() {
+    assertThat(slugGenerator.generateSlug(null)).isNull();
+  }
+
+  @DisplayName("returns an empty slug for an empty input")
+  @Test
+  public void returnsEmptyForEmpty() {
+    assertThat(slugGenerator.generateSlug("")).isEqualTo("");
+  }
+
+  @DisplayName("transforms to lowercase")
+  @Test
+  public void transformToLowercase() {
+    assertThat(slugGenerator.generateSlug("ABCDE")).isEqualTo("abcde");
+  }
+
+  @DisplayName("transforms umlauts to their standard form")
+  @Test
+  public void transformUmlautsToStandardForm() {
+    assertThat(slugGenerator.generateSlug("äÄ")).isEqualTo("aeae");
+  }
+
+  @DisplayName("transforms all but digits, characters and dashes to dashes")
+  @Test
+  public void transformToDashes() {
+    assertThat(slugGenerator.generateSlug("1/2 Kilo")).isEqualTo("1-2-kilo");
+  }
+
+  @DisplayName("strips dashes at the beginning and at the end")
+  @Test
+  public void noInitialAndFinalDashes() {
+    assertThat(slugGenerator.generateSlug("(foo bar)")).isEqualTo("foo-bar");
+  }
+
+  @DisplayName("avoids repeating dashes")
+  @Test
+  public void noRepeatingDashes() {
+    assertThat(slugGenerator.generateSlug("Paragraph $1")).isEqualTo("paragraph-1");
+  }
+
+}
